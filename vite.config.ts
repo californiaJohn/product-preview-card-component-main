c3ac6869a389904f8f4ff5320d33a26738680694
californiaJohn <jonathan.atia@is.com>
2022-09-15 23:54:03 +0300

    added repo base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/product-preview-card-component-main/tree/pages',
+	base: '/product-preview-card-component-main/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
