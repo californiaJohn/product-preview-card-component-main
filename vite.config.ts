388bfd001ca482e17babd2bce7fbf0dbc005ae14
californiaJohn <jonathan.atia@is.com>
2022-09-16 00:02:30 +0300

    trying again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// base: '/product-preview-card-component-main/',
+	base: '/product-preview-card-component-main/src/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
